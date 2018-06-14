d1603c2eb047c55eadf04c0e32507494d2c2b7eb
shortnd <ococncol@gmail.com>
2018-06-13 22:43:59 -0400

    added new form component

---
+++ resources/assets/js/components/FormComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-md-4">
+        <form>
+            <div class="form-group">
+                <textarea class="form-control" cols="8" rows="8" required></textarea>
+            </div><!--/.form-group-->
+            <div class="form-group">
+                <button class="btn btn-primary">Tweet</button>
+            </div><!--/.form-group-->
+        </form>
+    </div><!--/.col-md-4-->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
