ed01252dfb5a10f978fca6f5665af0b74c1cf752
shortnd <ococncol@gmail.com>
2018-06-14 23:08:06 -0400

    able to view messages now when pushed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <form-component></form-component>
-        <div class="col-md-8">
-            TimeLines
-        </div><!--/.col-md-8-->
-    </div><!--/.row.justify-content-center-->
-</div><!--/.container-->
+        <timeline-component></timeline-component>
+    </div>
+</div>
 @endsection
