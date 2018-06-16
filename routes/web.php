2a0e7409e12a6ac041062102fcdbedc34383d3d3
shortnd <ococncol@gmail.com>
2018-06-15 23:47:27 -0400

    users can not follow each other

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::post('tweet/save', 'PostController@store');
 Route::get('users/{user}', 'UserController@show')->name('user.show');
 Route::get('users/{user}/follow', 'UserController@follow')->name('user.follow');
 Route::get('users/{user}/unfollow', 'UserController@unfollow')->name('user.unfollow');
+Route::get('posts', 'PostController@index')->name('posts.index');
